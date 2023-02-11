9a3a0fbd3a2c116cff67a93c939cc0f5d43fe248
NaderAyed <nader.ayed@esprit.tn>
2023-02-11 16:06:34 +0100

    Product of Array Except Self

--- src/Solution.java
+++ src/Solution.java
@@ -9,6 +9,7 @@ public class Solution {
     To solve this problem, I use the Set interface, which doesn't accept duplicates.
     So as a result, we add a duplicate element to the set.
     the size of the set will be different from the size of the array.
+    Link:https://leetcode.com/problems/contains-duplicate/
     Complexity O(n)
     */
     public boolean containsDuplicate(int[] nums) {
@@ -31,6 +32,7 @@ public class Solution {
     Then create two maps where the key is the different char of the string
      and the value is the number of occurrences,
      finally compare the number of occurrences of the char if not equal return false otherwise return true
+     Link:https://leetcode.com/problems/valid-anagram/
      Complexity: O(n)
      Less complex solution, after transforming the strings into arrays
       just sort them and return Arrays.equals result
@@ -78,6 +80,7 @@ public class Solution {
      the map will hold as a key the actual element of nums, and as a value the index of element in nums
      if we found key = target - the current element if nums,
      then return nums's index and the other index from the map (value of target -nums[i]
+     Link:https://leetcode.com/problems/two-sum/
      Complexity : O(n)
      */
     public int[] twoSum(int[] nums, int target) {
@@ -105,7 +108,8 @@ public class Solution {
     single string and store them into a list,as a result we will get a groups of Anagrams,
     first of all  create a map that will be the dataStructure which store the lists as a value
     and about the Key it will be a reference string that will be used to put the anagrams together
-    finally as a return just take the map values and store them into an ArrayList .
+    finally as a return just take the map values and store them into an ArrayList.
+    Link:https://leetcode.com/problems/group-anagrams/
     Complexity : O(n)
 
      */
@@ -133,9 +137,11 @@ public class Solution {
      */
     public int[] productExceptSelf(int[] nums) {
         int[] answer = new int[nums.length];
-        int coif =1;
+        Arrays.fill(answer,1);
+        int postFix =1;
+        int preFix =1;
         //O(n²) solution
-  for (int i = 0; i < answer.length; i++) {
+ /* for (int i = 0; i < answer.length; i++) {
             coif=1;
             for (int j = 0; j < nums.length ; j++) {
                 if(j!=i){
@@ -143,6 +149,15 @@ public class Solution {
                 }
             }
             answer[i]=coif;
+        }*/
+        //after watching NeetCode explanation
+        for (int i = 0; i < nums.length; i++) {
+            answer[i]=preFix;
+            preFix*=nums[i];
+        }
+        for (int i = nums.length-1; i >=0 ; i--) {
+            answer[i]*=postFix;
+            postFix*=nums[i];
         }
 
         return answer;
