b4216a0110fc6eb2ad23ba359234f2eba9bf56e6
NaderAyed <nader.ayed@esprit.tn>
2023-02-14 19:52:01 +0100

    delete solution class

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -3,10 +3,7 @@ import java.util.Arrays;
 public class MainApplication {
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
-      int[] nums = {1,2,3,4};
-
-        System.out.println(Arrays.toString(solution.productExceptSelf(nums)));
+        //TODO Test Here!!
 
     }
 }
